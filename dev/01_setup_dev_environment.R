8357bc108a17b920a53a02cbd3031e1c3ab547b1
Brian Vancil <43145327+bvancilku@users.noreply.github.com>
2020-09-01 13:41:41 -0500

    Added setup and vis packages

---
+++ dev/01_setup_dev_environment.R
@@ -0,0 +1,9 @@
+renv::equip()
+renv::restore()
+
+## Run the following whenever you need to build:
+# drake::r_make()
+
+## Run to visualize the drake DAG:
+# drake::r_vis_drake_graph(targets_only = TRUE)
+# drake::r_sankey_drake_graph(targets_only = TRUE)
